632f6246f169f3a2c52e8fb31e782e6c1978c87e
Ahmet Kapusuz <ahmetkapusuz@gmail.com>
2020-05-03 00:51:57 +0200

    Apollo client integration

---
+++ src/pages/characters/[id].js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { withApollo } from '../../lib/apollo';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { useRouter } from 'next/router';
+
+export const CHARACTER_DETAIL = gql`
+  query CharacterDetail($id: ID) {
+    character(id: $id) {
+      id
+      name
+      status
+      type
+      image
+    }
+  }
+`;
+
+const CharacterDetail = () => {
+  const router = useRouter();
+  console.log('router.query', router.query);
+  const { id } = router.query;
+
+  const { loading, error, data } = useQuery(CHARACTER_DETAIL, {
+    variables: { id },
+    // Setting this value to true will make the component rerender when
+    // the "networkStatus" changes, so we are able to know if it is fetching
+    // more data
+    notifyOnNetworkStatusChange: false,
+  });
+
+  if (error) return <div>Error fetching data</div>;
+  if (loading) return <div>Loading</div>;
+
+  const { character } = data;
+
+  return (
+    <div>
+      {character && (
+        <>
+          <img src={character.image} alt="Character image" />
+          <div>
+            <span>{character.id}</span> - <span>{character.name}</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default withApollo({ ssr: true })(CharacterDetail);
--- src/pages/characters/index.js
+++ src/pages/characters/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import Link from 'next/link';
+import { request } from 'graphql-request';
 
-const Characters = () => {
-  return <div>Characters</div>;
+const ALL_CHARACTERS = `
+  query AllCharacters($page: Int) {
+    current: characters(page: $page) {
+      results {
+        id
+        name
+      }
+    }
+  }
+`;
+
+const Characters = ({ data }) => {
+  return (
+    <div>
+      <ul>
+        {data &&
+          data.map((char) => {
+            return (
+              <li key={`character-${char.id}`}>
+                <Link href={`/characters/${char.id}`}>{char.name}</Link>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  // const data = { id: 1, name: 'Rick' };
+
+  const response = await request(
+    'https://rickandmortyapi.com/graphql/',
+    ALL_CHARACTERS,
+    {
+      id: 1,
+    }
+  );
+
+  console.log('response', response);
+  const data = response.current.results;
+
+  return {
+    props: { data },
+  };
+}
 
 export default Characters;

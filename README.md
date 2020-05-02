540b54d3e6a9d60c8a58d7bc0a10f8e86d39124f
Ahmet Kapusuz <ahmetkapusuz@gmail.com>
2020-05-02 14:52:31 +0200

    initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# NextJS Boilerplate
+
+This is a NextJS Boilerplate application to make it easy to start creating awesome server side rendered React web applications.
---
+++ src/components/HelloWorld/HelloWorld.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import HelloWorld from './';
+
+export default { title: 'Hello World' };
+
+export const simpleComponent = () => <HelloWorld />;

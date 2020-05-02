4fc43b8457b7a93858691be3c66dd2199af3d0f4
Ahmet Kapusuz <ahmetkapusuz@gmail.com>
2020-05-02 23:23:32 +0200

    Layout component setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+import styled from 'styled-components';
 import HelloWorld from '../components/HelloWorld';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
 export default () => (
-  <div>
-    <h1>Simple Storybook Example</h1>
+  <Container>
+    <h1>Next.js Boilerplate</h1>
     <HelloWorld />
-  </div>
+    <Link href="/characters">Characters</Link>
+  </Container>
 );
